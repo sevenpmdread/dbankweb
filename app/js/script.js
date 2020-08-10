e801ce77bd38e3edace9dd45e8d87a8f9d82de1e
unknown <aditya.sangal13@gmail.com>
2020-08-10 16:19:42 +0530

    first commit

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,37 @@
 const header = document.querySelector(".header");
+const hamburger = document.querySelector("#hambtn");
+const animate = document.querySelector(".overlay");
+const fadeelems = document.querySelectorAll(".has-fade");
+const body = document.querySelector("body");
 
-
-
-header.addEventListener("click", function()
+hamburger.addEventListener("click", function()
 {
 if(header.classList.contains("open"))
 {
+    //remove overlay
+    //close hamburger
+    body.classList.remove("noscroll");
  header.classList.remove("open");
-
-   
+ fadeelems.forEach(function(elements)
+    {
+elements.classList.add("fade-out");
+ elements.classList.remove("fade-in");
+    });
 }
+   
+
 else
-{
+{ 
+    //open hamburger
+    body.classList.add("noscroll");
+  
     header.classList.add("open");
+
+    fadeelems.forEach(function(elements)
+    { elements.classList.add("fade-in");
+    elements.classList.remove("fade-out");
+
+    });
   
 }
 
